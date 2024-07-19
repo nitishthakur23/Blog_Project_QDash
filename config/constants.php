1174780d74212260ac61be5aff011212929cb894
Nitish Thakur <160464604+nitishthakur23@users.noreply.github.com>
2024-07-19 19:08:50 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 define('ROOT_URL','http://localhost/Blog/');
-define('DB_HOST','localhost');
-define('DB_USER','user_name');
-define('DB_PASS','password');
-define('DB_NAME','blog');
+define('DB_HOST','DB_HOST');
+define('DB_USER','DB_USER');
+define('DB_PASS','DB_PASS');
+define('DB_NAME','DB_NAME');
